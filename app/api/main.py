6743572c4cb2842f1b8956f9ffe064a4a9b56594
Carlos Ivan <93873199+camunozv@users.noreply.github.com>
2025-11-17 18:47:29 -0500

    Create access token funciton

--- app/api/main.py
+++ app/api/main.py
@@ -1,11 +1,23 @@
-from fastapi import FastAPI
-#from app.api.v1.router import api_router
-#from app.core.config import settings
+from fastapi import Depends, FastAPI, HTTPException, status
+# from app.api.v1.router import api_router
+# from app.core.config import settings
+
+
+#@app.get("/test/{hello_there}/")
+#async def test_endpoint(hello_there: str, query_param_1: int):
+   # return {"hello": hello_there, "your param is ": query_param_1}
+
+
+# Use pydantic models for automatically validating the data you are
+# receiving
+#@app.post("/create_user")
+#async def test_endpoint2(new_user: User):  # In this way the endpoints
+    # know that they have to receive data in JSON format
+    #return new_user
+
 
 app = FastAPI(
-    title="Diffusion Maps initial version",
-    #version=settings.VERSION,
-    #openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    title="Diffusion Maps v1",
+    # version=settings.VERSION,
+    # openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
-
-#app.include_router(api_router, prefix=settings.API_V1_STR)
